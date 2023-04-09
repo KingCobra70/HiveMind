6ffa504ce0233aeea7b7b552fadb51eedfcb2a8c
KingCobra70 <edellenburg@yahoo.com>
2023-04-09 12:31:37 -0400

    1.0.5

--- ai_hivemind.py
+++ ai_hivemind.py
@@ -7,17 +7,47 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
 def ingest_data_from_huggingface_api(model_name, api_key, input_text):
-    # Your code to fetch and process data from the HuggingFace API
-    pass
+    api_url = f"https://api-inference.huggingface.co/models/{model_name}"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    data = json.dumps({"inputs": input_text})
+    
+    response = requests.post(api_url, headers=headers, data=data)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Error fetching data from Hugging Face API: {response.status_code}")
 
 def preprocess_data(data):
-    # Your code to preprocess the data
-    pass
+    # Assuming that the API response has a field called 'generated_text'
+    # You may need to adjust this based on the actual API response structure
+    output_text = data.get("generated_text")
+    preprocessed_data = {"output_text": output_text}
+    
+    return preprocessed_data
 
 def store_data_in_database(preprocessed_data, table_name):
-    # Your code to store the preprocessed data in the database
-    pass
+    conn = sqlite3.connect('huggingface_results.db')
+    c = conn.cursor()
+
+    c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, output_text TEXT NOT NULL)")
+
+    c.execute(f"INSERT INTO {table_name} (output_text) VALUES (?)", (preprocessed_data["output_text"],))
+
+    conn.commit()
+    conn.close()
 
 def cluster_data():
-    # Your code to cluster the data using K-Means
-    pass
+    conn = sqlite3.connect('huggingface_results.db')
+    df = pd.read_sql_query("SELECT * FROM huggingface_results", conn)
+    conn.close()
+
+    vectorizer = TfidfVectorizer(stop_words="english")
+    X = vectorizer.fit_transform(df["output_text"])
+
+    kmeans = KMeans(n_clusters=3, random_state=42)
+    kmeans.fit(X)
+
+    df["cluster"] = kmeans.labels_
+
+    return df
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
 import os
 import sqlite3
 import pandas as pd
@@ -6,35 +7,93 @@ from ai_hivemind import ingest_data_from_huggingface_api, preprocess_data, store
 
 app = Flask(__name__)
 
+# Define a function to create a table in SQLite database
 def create_table():
-    # Your create_table function code here
+    conn = sqlite3.connect('responses.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS responses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            question TEXT NOT NULL,
+            response TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
+# Call create_table function to create a table in the database
 create_table()
 
-def store_question_and_response(question, response):
-    # Your store_question_and_response function code here
+# Add other functions here
 
-def find_consensus(responses):
-    # Your find_consensus function code here
+def store_question_and_response(question, response):
+    conn = sqlite3.connect('responses.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO responses (question, response) VALUES (?, ?)', (question, response))
+    conn.commit()
+    conn.close()
 
 def model_interaction(input_text):
-    # Your model_interaction function code here
-    # Make sure to call the process_model_a, process_model_b, and process_model_c functions
-    # and the model_response function as needed
+    model_name = "bert-base-cased"
+    api_key = os.getenv("HUGGINGFACE_API_KEY")
 
+    # Ingest data from Hugging Face API
+    data = ingest_data_from_huggingface_api(model_name, api_key, input_text)
+
+    # Preprocess data
+    preprocessed_data = preprocess_data(data)
+
+    # Store data in SQLite database
+    store_data_in_database(preprocessed_data, "huggingface_results")
+
+    # Extract output_text from preprocessed_data
+    output_text = preprocessed_data["output_text"]
+
+    return output_text
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Define the index route
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # Your index function code here
+    if request.method == 'POST':
+        input_text = request.form.get('input_text')
+        input_image = request.files.get('input_image')
+        input_audio = request.files.get('input_audio')
+        input_video = request.files.get('input_video')
 
+        # Save uploaded files to disk
+        if input_image:
+            filename = secure_filename(input_image.filename)
+            input_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if input_audio:
+            filename = secure_filename(input_audio.filename)
+            input_audio.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if input_video:
+            filename = secure_filename(input_video.filename)
+            input_video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Process input_text, input_image, input_audio, input_video as needed
+
+        return redirect(url_for('results'))
+    else:
+        return render_template('index.html')
+# Define the results route
 @app.route("/results")
 def results():
-    # Your results function code here
+    conn = sqlite3.connect('responses.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM responses')
+    responses = c.fetchall()
+    conn.close()
+    return render_template('results.html', responses=responses)
 
+# Define the interact route to handle POST requests from the frontend
 @app.route('/interact', methods=['POST'])
 def interact():
     input_text = request.json.get('input_text')
     output_text = model_interaction(input_text)
     return jsonify({'output_text': output_text})
 
+# Run the Flask app if this is the main module
 if __name__ == "__main__":
     app.run(debug=True)
